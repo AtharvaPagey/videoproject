2fb1e127fc22869fc8770c404e5c3211537d89c8
AtharvaPagey <163169996+AtharvaPagey@users.noreply.github.com>
2025-02-02 19:54:38 +0530

    added changes to the index

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import {DB_NAME} from './constants.js';
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+/*
+// Approach-1:
+
+import express from 'express'
+const app = express()
+
+
+
+;(async()=>{// this is called an ' IFFY '-> this will rrun immediately without any calling...(professional appraoch)
+            //  always starts with a ';' as there are chances that previous line may or maynot have a ';'
+
+            try{
+                await mongoose.connect(`${process.env.MONGODB_URI}
+                    /${DB_NAME}`);
+                app.on("error",(error)=>{
+                    console.log("ERROR:",error);
+                    throw error;
+                })    
+
+                app.listen(process.env.PORT,()=>{
+                    console.log(`App is listening on port ${process.env.PORT}`)
+                })
+                
+            }
+            catch(error){
+               console.log("ERROR:", error)
+               throw error 
+            }
+})() 
+*/
