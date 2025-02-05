e7ce6478c8755ecb560176362894580f59c17c55
AtharvaPagey <163169996+AtharvaPagey@users.noreply.github.com>
2025-02-05 19:35:59 +0530

    learnt abt JWT and bcrypt

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
-import dotenv from 'dotenv'
-import mongoose from 'mongoose';
-import {DB_NAME} from './constants.js';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
-.then(()=>{
-    app.on("error",(error)=>{
-        console.log("ERROR:",error);
-        throw error;
-    })
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is runnning at port: ${process.env.PORT}`);
-    })
-})
-.catch((error)=>{
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runnning at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log("Mongo DB connection Failed:", error);
-})
-
-
-
-
-
-
+  });
 
 /* 
 // Approach-1:
